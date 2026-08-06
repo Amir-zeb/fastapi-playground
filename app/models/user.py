7cd9e9691ed69607641983d6ef7e3117a8af48cd
Amir-zeb <amirzeb695@gmail.com>
2026-08-06 15:56:46 +0500

    add pass field to user model

--- app/models/user.py
+++ app/models/user.py
@@ -10,5 +10,6 @@ class UserModel(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String)
     email: Mapped[str] = mapped_column(String, unique=True)
+    password: Mapped[str] = mapped_column(String)
     age: Mapped[int] = mapped_column(Integer)
     gender: Mapped[str] = mapped_column(String)
