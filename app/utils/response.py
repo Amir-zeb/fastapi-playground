85a04c5b62e6188982c7d0520333e1af6f69e7d6
Amir-zeb <amirzeb695@gmail.com>
2026-08-08 16:26:28 +0500

    update response helper

--- app/utils/response.py
+++ app/utils/response.py
@@ -3,8 +3,11 @@
 from fastapi import status
 
 def success_response(message: str, data=None, status_code=status.HTTP_200_OK):
+    if data is None:
+        data = {}
     return {
         "status": status_code,
         "message": message,
         "data": data,
     }
+    
